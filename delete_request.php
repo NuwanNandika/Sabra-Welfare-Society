cae84d2db117262bc14c1bc98c65fba5203423d2
Buwaneka Sudheera <Buwaneka Sudheera>
2017-07-13 00:18:36 +0530

    First Commit

--- delete_request.php
+++ delete_request.php
@@ -15,6 +15,17 @@ if (!$admin) {
     redirect_to("request_list.php");
 }
 $id = $admin["id"];
+$sql = "SELECT * FROM request WHERE id = {$id}";
+$sql_set = mysqli_query($conn, $sql);
+$member = mysqli_fetch_assoc($sql_set);
+$aid = $member["id"];
+$username = $member["uname"];
+$reason = $member["reason"];
+$money = $member["money"];
+$other = $member["other"];
+
+$insert = "INSERT INTO given(id,uname,reason,money,other) VALUES('$aid','$username','$reason','$money','$other')";
+$insert_set = mysqli_query($conn, $insert);
 $query = "DELETE FROM request WHERE id = {$id} LIMIT 1";
 $result = mysqli_query($conn, $query);
 
